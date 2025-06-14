4b21c19949461e7621400d5e1519c41ecc15c3a6
Omnyamohamed22393 <omnyamohammed642@gmail.com>
2025-06-14 23:28:31 +0300

    Use Control flow  @for  in Portfolio & Skills Sections

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.scss'
 })
 export class Portfolio {
-
+  portfolioItems = [
+    'Web Design',
+    'Responsive Design',
+    'Mobile Design',
+    'UI/UX Design',
+    'Flutter App',
+    'Cloud Computing'
+  ];
 }
--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
 export class Skills {
+  focusItems = [
+    'UI/UX Design',
+    'Web Design',
+    'Responsive Design',
+    'Mobile Design'
+  ];
 
+  skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Typescript',
+    'Sass',
+    'Figma',
+    'React',
+    'Angular'
+  ];
 }
